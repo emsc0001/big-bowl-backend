d880670bf8cfccf9171566ee2600c7dd5f5371f0
Abdiox <abdirahmanroble2610@gmail.com>
2024-05-14 15:31:42 +0200

    added service

--- src/main/java/bigbowl/employees/EmployeeService.java
+++ src/main/java/bigbowl/employees/EmployeeService.java
@@ -1,4 +1,41 @@
 package bigbowl.employees;
 
+import bigbowl.employees.Employee;
+import bigbowl.employees.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+
+    @Autowired
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    public Employee getEmployeeById(Long id) {
+        return employeeRepository.findById(id).orElse(null);
+    }
+
+    public Employee createEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    public Employee updateEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    public void deleteEmployee(Long id) {
+        employeeRepository.deleteById(id);
+    }
 }
+
+
