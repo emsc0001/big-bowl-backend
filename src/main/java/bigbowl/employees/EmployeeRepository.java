effd3e3db617774092afb02ac440ac10700b9641
Abdiox <abdirahmanroble2610@gmail.com>
2024-05-14 15:32:14 +0200

    added repository

--- src/main/java/bigbowl/employees/EmployeeRepository.java
+++ src/main/java/bigbowl/employees/EmployeeRepository.java
@@ -1,4 +1,9 @@
 package bigbowl.employees;
 
-public interface EmployeeRepository {
+import bigbowl.employees.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    // Additional custom query methods if needed
 }
+
---
+++ src/main/java/bigbowl/employees/EmployeeRole.java
@@ -0,0 +1,4 @@
+package bigbowl.employees;
+
+public class EmployeeRole {
+}
